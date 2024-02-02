c941e70b3690357d824da1992fba18ebf8a9284b
Apoorv1180 <apoorv111221cse@gmail.com>
2024-02-01 23:50:49 -0500

    refactor and add comments.

--- feature/news/ui/src/main/java/com/feature/news/ui/navigation/screen/NewsStateHolder.kt
+++ feature/news/ui/src/main/java/com/feature/news/ui/navigation/screen/NewsStateHolder.kt
@@ -2,8 +2,14 @@ package com.feature.news.ui.navigation.screen
 
 import com.feature.news.domain.model.News
 
+// Data class representing the state of the news list in the ViewModel
 data class NewsStateHolder(
+    // Flag indicating whether the news list is currently being loaded
     val isLoading: Boolean = false,
+
+    // List of news data (nullable)
     val data: List<News>? = null,
+
+    // Error message (empty string by default)
     val error: String = ""
 )
